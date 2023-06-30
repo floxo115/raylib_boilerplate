255644a648f59de22f3322ca6d546025a4ca62ef
floxo115 <flojak@gmx.net>
2023-06-30 21:35:54 +0200

    added music

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <raylib.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    InitWindow(800, 600, "Raylib Window");
+    SetTargetFPS(60);
+
+    InitAudioDevice();
+    Music music = LoadMusicStream("../media/music.mp3");
+    PlayMusicStream(music);
+    int textPos = 0;
+    Color colorPallette[] {RED, LIGHTGRAY, BLUE, GREEN, YELLOW, MAGENTA};
+    while (!WindowShouldClose()) {
+        UpdateMusicStream(music);
+
+        textPos = textPos % 800;
+        BeginDrawing();
+        ClearBackground(DARKPURPLE);
+        DrawFPS(30, 30);
+
+
+        DrawText("Hello, Raylib!", textPos++, 300, 70, colorPallette[textPos % std::size(colorPallette)]);
+        DrawText("Hello, Raylib!", textPos++ - 800, 300, 70, colorPallette[textPos % std::size(colorPallette)]);
+        EndDrawing();
+    }
 }
